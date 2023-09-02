61b56af06bcfa45ca625cbac0e25ca47bf1f8708
junreythedatadigger <junrey-o-ansing@gmail.com>
2023-09-02 11:39:44 +0800

    Added comments for explanations

--- retail-sale-datasets/costumer-demographics-dataset-generator.py
+++ retail-sale-datasets/costumer-demographics-dataset-generator.py
@@ -14,8 +14,8 @@ customers = []
 
 for _ in range(num_customers):
     customer_id = f'CUST{"{:05d}".format(_)}'                           # Added leading zeros
-    name = fake.unique.name()
-    age = random.randint(18, 70)
+    name = fake.unique.name()                                           # Generate a fake name
+    age = random.randint(18, 70)                                        # Generate a random age
     gender = gender_type.get_gender(name.split()[0]).capitalize()       # Get the gender of first name at index 0
     if gender == 'Unknown':                                             # Leading title like Dr., Mrs, etc
         gender = gender_type.get_gender(name.split()[1]).capitalize()   # Get the gender of first name at index 1
@@ -24,9 +24,9 @@ for _ in range(num_customers):
         gender = 'Female'                                               # Set to 'Female'
     elif gender == 'Mostly_male':                                       # If returns 'Mostly_male
         gender = 'Male'                                                 # Set to 'Male'
-    location = fake.city()
+    location = fake.city()                                              # Generate a fake city
     membership_type = fake.random_element(['Silver', 'Gold', 'Platinum'])
     customers.append([customer_id, name, age, gender, location, membership_type])
 
-customers_df = pd.DataFrame(customers, columns=['Customer ID', 'Name', 'Age', 'Gender', 'Location', 'Membership Type'])
+customers_df = pd.DataFrame(customers, columns=['CustomerID', 'Name', 'Age', 'Gender', 'Location', 'MembershipType'])
 customers_df.to_csv('customer_demographics.csv', index=False)
