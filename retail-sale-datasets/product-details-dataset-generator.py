1f600ce55d1b73846a33a39c83ec182e3ee2d440
junreythedatadigger <junrey-o-ansing@gmail.com>
2023-09-02 08:48:28 +0800

    Implemented dynamic provider for product details

--- retail-sale-datasets/product-details-dataset-generator.py
+++ retail-sale-datasets/product-details-dataset-generator.py
@@ -1,27 +1,32 @@
 import pandas as pd
-import random
 from faker import Faker
+from faker.providers import DynamicProvider
 
-from product_details import Categories, Subcategories
+from product_details import Product_Details
 
-categories = Categories
-subcategories = Subcategories
+product_details_list = Product_Details
+
+product_details_provider = DynamicProvider(
+    provider_name = "product_detail",
+    elements = product_details_list,
+)
 
 fake = Faker()
 
-num_products = 50  # Adjust as needed
+fake.add_provider(product_details_provider)
+
+num_products = 500  # Adjust as needed
 
 products = []
 
 for _ in range(num_products):
-    product_id = f'PROD{"{:05d}".format(_)}' # Added leading zeros
-    product_name = fake.word()
-    # category = fake.random_element(['Electronics', 'Clothing', 'Furniture'])
-    category = fake.random_element(categories)
-    # subcategory = fake.random_element(['Computers', 'Shirts', 'Chairs'])
-    subcategory = fake.random_element(subcategories[category])
-    manufacturer = fake.company()
-    unit_cost = random.uniform(50, 1000)
+    product_id = f'PROD{"{:05d}".format(_)}'            # Added leading zeros
+    product_detail = fake.product_detail()              # Store the random product detail
+    product_name = product_detail['product name']       # Get the product name
+    category = product_detail['category']               # Get the category
+    subcategory = product_detail['subcategory']         # Get the subcategory
+    manufacturer = fake.company()                       # Generate fake company name
+    unit_cost = product_detail['unit price']            # Get the unit price
     products.append([product_id, product_name, category, subcategory, manufacturer, unit_cost])
 
 products_df = pd.DataFrame(products, columns=['Product ID', 'Product Name', 'Category', 'Subcategory', 'Manufacturer', 'Unit Cost'])
